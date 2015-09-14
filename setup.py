2984c6db9427e2af0cbb9eb9999b93c76e6af85b
Peter Bengtsson <mail@peterbe.com>
2015-09-14 13:55:41 -0700

    warming up

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='headsupper',
+      version='0.1.0',
+      description='This is https://github.com/peterbe/headsupper',
+      author='Peter Bengtsson',
+      author_email='mail@peterbe.com',
+      url='https://github.com/peterbe/headsupper')
