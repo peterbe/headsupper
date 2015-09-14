6e9a2152b60312c7864024719bd7c39e9cd1015b
Peter Bengtsson <mail@peterbe.com>
2015-09-14 14:25:12 -0700

    signature

--- headsupper/base/views.py
+++ headsupper/base/views.py
@@ -1,11 +1,13 @@
 import json
 import os
 import time
+import hashlib, base64, hmac
 
 from django import http
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
+SECRET = "My Secret"
 
 @csrf_exempt
 def home(request):
@@ -17,14 +19,22 @@ def home(request):
         settings.MEDIA_ROOT,
         '%s.json' % int(time.time())
     )
-    print request.META.keys()
-    for key in request.META.keys():
-        if not (key.startswith('HTTP') or 'X' in key):
-            continue
-        print key
-        print "\t", str(request.META[key])[:100]
-        print
-    body = json.loads(request.body)
+    # print request.META.keys()
+    # for key in request.META.keys():
+    #     if not (key.startswith('HTTP') or 'X' in key):
+    #         continue
+    #     print key
+    #     print "\t", str(request.META[key])[:100]
+    #     print
+
+    github_signature = request.META['HTTP_X_GITHUB_DELIVERY']
+    print "GITHUB_SIGNATURE", repr(github_signature)
+    payload = request.body
+    signature = hmac.new(SECRET, payload, hashlib.sha256)
+    print "SIGNATURE", repr(signature)
+    print hmac.compare_digest(signature, github_signature)
+
+    body = json.loads(payload)
     # print repr(request.body)
     with open(dbg_filename, 'w') as f:
         json.dump(body, f, indent=2)
